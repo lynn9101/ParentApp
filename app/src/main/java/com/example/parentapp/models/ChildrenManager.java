6fe9e02663ddf1582cda4db33680b5a32b2aa284
Jla787 <Jla787@sfu.ca>
2021-10-24 16:40:06 -0700

    models/ChildrenManager: added logic to handle insertion, modification & deletion of Child object

--- app/src/main/java/com/example/parentapp/models/ChildrenManager.java
+++ app/src/main/java/com/example/parentapp/models/ChildrenManager.java
@@ -1,7 +1,10 @@
 package com.example.parentapp.models;
 
+import java.util.ArrayList;
+
 public class ChildrenManager {
     private static ChildrenManager childrenManager;
+    private ArrayList<Child> children;
 
     public static ChildrenManager getInstance() {
         if (childrenManager == null) {
@@ -12,6 +15,44 @@ public class ChildrenManager {
     }
 
     private ChildrenManager() {
+        this.children = new ArrayList<>();
+    }
 
+    public ArrayList<Child> getChildren() {
+        return this.children;
+    }
+
+    public Child getChild(int childId) {
+        if (childId >= this.children.size()) {
+            throw new IllegalArgumentException("Child ID: " + childId + " exceeds children count.");
+        }
+
+        if (this.children.get(childId) == null) {
+            throw new IllegalArgumentException("Child ID: " + childId + " not found.");
+        }
+
+        return this.children.get(childId);
+    }
+
+    public Child addChild(Child child) {
+        this.children.add(child);
+        return child;
+    }
+
+    public Child updateChild(int index, Child child) {
+        this.children.set(index, child);
+        return child;
+    }
+
+    public void removeChild(int childId) {
+        if (childId >= this.children.size()) {
+            throw new IllegalArgumentException("Child ID: " + childId + " exceeds children count.");
+        }
+
+        if (this.children.get(childId) == null) {
+            throw new IllegalArgumentException("Child ID: " + childId + " not found.");
+        }
+
+        this.children.remove(childId);
     }
 }
