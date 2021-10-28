97b750d63d6a6a18e9dff5732b27e07caa3ff1c5
wla161 <wla161@sfu.ca>
2021-10-28 14:50:53 -0700

    TimerActivity: Replaced all magic numbers with constants and added function to handle the case when timer runs out of time and then user can press "Reset" button to reset time to the last selected starting time Integers.xml: deleted line 4 from old version

--- app/src/main/java/com/example/parentapp/TimerActivity.java
+++ app/src/main/java/com/example/parentapp/TimerActivity.java
@@ -20,13 +20,17 @@ public class TimerActivity extends AppCompatActivity {
     private ImageButton btnReset;
     private ImageButton btnPause;
     private ImageButton btnResume;
+    private ImageButton btnResetWhenStop;
     private Button btn1Min;
     private Button btn2Min;
     private Button btn3Min;
     private Button btn5Min;
     private Button btn10Min;
     private ImageView calmDown;
-    private final long DEFAULT_DISPLAY_MILLIS = 600000;
+    private ImageView timeIsUp;
+    private final long DEFAULT_DISPLAY_MILLIS = 10000;
+    private final int COUNTDOWN_INTERVAL = 1000;
+    private final int SIXTY = 60;
     private boolean isRunning = false;
     private CountDownTimer countDownTimer;
     private long timeLeftMills = DEFAULT_DISPLAY_MILLIS;
@@ -48,7 +52,7 @@ public class TimerActivity extends AppCompatActivity {
         btnPause = findViewById(R.id.imgBtnPause);
         btnResume = findViewById(R.id.imgBtnResume);
         btnStart.setVisibility(View.VISIBLE);
-
+        btnResetWhenStop = findViewById(R.id.imgBtnResetWhenStop);
         btn1Min = findViewById(R.id.btn1min);
         btn2Min = findViewById(R.id.btn2min);
         btn3Min = findViewById(R.id.btn3min);
@@ -56,9 +60,11 @@ public class TimerActivity extends AppCompatActivity {
         btn10Min = findViewById(R.id.btn10min);
 
         calmDown = findViewById(R.id.imgCalmDown); // Resource:https://www.clipartmax.com/middle/m2i8K9m2b1G6K9m2_calm-clip-art/
+        timeIsUp = findViewById(R.id.imgTimeIsUp); // https://www.clipartmax.com/middle/m2i8N4b1m2N4d3Z5_when-the-district-operates-under-a-90-minute-delay-alarm-clock-clip/
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                isRunning = true;
                 startTimer();
                 calmDown.setVisibility(View.VISIBLE);
                 btnStart.setVisibility(View.INVISIBLE);
@@ -74,6 +80,7 @@ public class TimerActivity extends AppCompatActivity {
         btnResume.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                isRunning = true;
                 startTimer();
                 btnResume.setVisibility(View.INVISIBLE);
                 btnPause.setVisibility(View.VISIBLE);
@@ -85,6 +92,7 @@ public class TimerActivity extends AppCompatActivity {
         btnPause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                isRunning = false;
                 pauseTimer();
                 btnPause.setVisibility(View.INVISIBLE);
                 btnResume.setVisibility(View.VISIBLE);
@@ -96,37 +104,38 @@ public class TimerActivity extends AppCompatActivity {
         btnReset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                isRunning = false;
                 resetTimer();
                 pauseTimer();
-                calmDown.setVisibility(View.INVISIBLE);
-                btnStart.setVisibility(View.VISIBLE);
-                btnPause.setVisibility(View.INVISIBLE);
-                btnReset.setVisibility(View.INVISIBLE);
-                btn1Min.setVisibility(View.VISIBLE);
-                btn2Min.setVisibility(View.VISIBLE);
-                btn3Min.setVisibility(View.VISIBLE);
-                btn5Min.setVisibility(View.VISIBLE);
-                btn10Min.setVisibility(View.VISIBLE);
+            }
+        });
+
+        btnResetWhenStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btnResetWhenStop.setVisibility(View.INVISIBLE);
+                timeIsUp.setVisibility(View.INVISIBLE);
+                resetTimer();
             }
         });
 
         refreshCountDownText();
     }
 
-
     private void startTimer() {
-        countDownTimer = new CountDownTimer(timeLeftMills,1000) {
+        countDownTimer = new CountDownTimer(timeLeftMills,COUNTDOWN_INTERVAL) {
             @Override
             public void onTick(long l) {
                 timeLeftMills = l;
                 refreshCountDownText();
+                isTimerEnd();
             }
 
             @Override
             public void onFinish() {
                 isRunning = false;
-                btnStart.setVisibility(View.INVISIBLE);
-                btnReset.setVisibility(View.VISIBLE);
+                btnStart.setVisibility(View.VISIBLE);
+                btnReset.setVisibility(View.INVISIBLE);
             }
         }.start();
         isRunning = true;
@@ -138,8 +147,16 @@ public class TimerActivity extends AppCompatActivity {
     private void resetTimer() {
         timeLeftMills = DEFAULT_DISPLAY_MILLIS;
         refreshCountDownText();
+        calmDown.setVisibility(View.INVISIBLE);
+        btnStart.setVisibility(View.VISIBLE);
+        btnPause.setVisibility(View.INVISIBLE);
         btnReset.setVisibility(View.INVISIBLE);
         btnResume.setVisibility(View.INVISIBLE);
+        btn1Min.setVisibility(View.VISIBLE);
+        btn2Min.setVisibility(View.VISIBLE);
+        btn3Min.setVisibility(View.VISIBLE);
+        btn5Min.setVisibility(View.VISIBLE);
+        btn10Min.setVisibility(View.VISIBLE);
     }
 
     private void pauseTimer() {
@@ -148,9 +165,22 @@ public class TimerActivity extends AppCompatActivity {
     }
 
     private void refreshCountDownText() {
-        int minutes = (int) timeLeftMills / 1000 / 60 ;
-        int seconds = (int) timeLeftMills / 1000 % 60;
+        int minutes = (int) timeLeftMills / COUNTDOWN_INTERVAL / SIXTY ;
+        int seconds = (int)  timeLeftMills / COUNTDOWN_INTERVAL % SIXTY;
         String displayTimeLeft = String.format(Locale.getDefault(),"%02d:%02d", minutes, seconds);
         txtTimeCountDown.setText(displayTimeLeft);
     }
+
+    public void isTimerEnd(){
+        if (timeLeftMills < 1000) {
+            btnResetWhenStop.setVisibility(View.VISIBLE);
+            timeIsUp.setVisibility(View.VISIBLE);
+            calmDown.setVisibility(View.INVISIBLE);
+            btnStart.setVisibility(View.INVISIBLE);
+            btnPause.setVisibility(View.INVISIBLE);
+            btnReset.setVisibility(View.INVISIBLE);
+            btnResume.setVisibility(View.INVISIBLE);
+        }
+    }
+
 }
