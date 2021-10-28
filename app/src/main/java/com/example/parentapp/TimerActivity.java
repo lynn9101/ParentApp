3350edc0f0d5cf7dbe35db35150db71a4da72828
wla161 <wla161@sfu.ca>
2021-10-27 22:01:01 -0700

    TimerActivity: adjusted the layout to compatible with different size of screen and added pre-set interval buttons

--- app/src/main/java/com/example/parentapp/TimerActivity.java
+++ app/src/main/java/com/example/parentapp/TimerActivity.java
@@ -9,6 +9,7 @@ import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.Locale;
@@ -19,10 +20,16 @@ public class TimerActivity extends AppCompatActivity {
     private ImageButton btnReset;
     private ImageButton btnPause;
     private ImageButton btnResume;
-    private static final long DEFAULT_DISPLAY_TIMER_MILLIS = 600000;
+    private Button btn1Min;
+    private Button btn2Min;
+    private Button btn3Min;
+    private Button btn5Min;
+    private Button btn10Min;
+    private ImageView calmDown;
+    private final long DEFAULT_DISPLAY_MILLIS = 600000;
     private boolean isRunning = false;
     private CountDownTimer countDownTimer;
-    private long timeLeftMills = DEFAULT_DISPLAY_TIMER_MILLIS;
+    private long timeLeftMills = DEFAULT_DISPLAY_MILLIS;
 
     public static Intent makeIntent(Context context) {
         return new Intent(context,TimerActivity.class);
@@ -42,12 +49,25 @@ public class TimerActivity extends AppCompatActivity {
         btnResume = findViewById(R.id.imgBtnResume);
         btnStart.setVisibility(View.VISIBLE);
 
+        btn1Min = findViewById(R.id.btn1min);
+        btn2Min = findViewById(R.id.btn2min);
+        btn3Min = findViewById(R.id.btn3min);
+        btn5Min = findViewById(R.id.btn5min);
+        btn10Min = findViewById(R.id.btn10min);
+
+        calmDown = findViewById(R.id.imgCalmDown); // Resource:https://www.clipartmax.com/middle/m2i8K9m2b1G6K9m2_calm-clip-art/
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startTimer();
+                calmDown.setVisibility(View.VISIBLE);
                 btnStart.setVisibility(View.INVISIBLE);
                 btnReset.setVisibility(View.VISIBLE);
+                btn1Min.setVisibility(View.INVISIBLE);
+                btn2Min.setVisibility(View.INVISIBLE);
+                btn3Min.setVisibility(View.INVISIBLE);
+                btn5Min.setVisibility(View.INVISIBLE);
+                btn10Min.setVisibility(View.INVISIBLE);
             }
         });
 
@@ -78,9 +98,15 @@ public class TimerActivity extends AppCompatActivity {
             public void onClick(View view) {
                 resetTimer();
                 pauseTimer();
+                calmDown.setVisibility(View.INVISIBLE);
                 btnStart.setVisibility(View.VISIBLE);
                 btnPause.setVisibility(View.INVISIBLE);
                 btnReset.setVisibility(View.INVISIBLE);
+                btn1Min.setVisibility(View.VISIBLE);
+                btn2Min.setVisibility(View.VISIBLE);
+                btn3Min.setVisibility(View.VISIBLE);
+                btn5Min.setVisibility(View.VISIBLE);
+                btn10Min.setVisibility(View.VISIBLE);
             }
         });
 
@@ -110,7 +136,7 @@ public class TimerActivity extends AppCompatActivity {
 
 
     private void resetTimer() {
-        timeLeftMills = DEFAULT_DISPLAY_TIMER_MILLIS;
+        timeLeftMills = DEFAULT_DISPLAY_MILLIS;
         refreshCountDownText();
         btnReset.setVisibility(View.INVISIBLE);
         btnResume.setVisibility(View.INVISIBLE);
@@ -122,7 +148,7 @@ public class TimerActivity extends AppCompatActivity {
     }
 
     private void refreshCountDownText() {
-        int minutes = (int) timeLeftMills / 1000 / 60;
+        int minutes = (int) timeLeftMills / 1000 / 60 ;
         int seconds = (int) timeLeftMills / 1000 % 60;
         String displayTimeLeft = String.format(Locale.getDefault(),"%02d:%02d", minutes, seconds);
         txtTimeCountDown.setText(displayTimeLeft);
