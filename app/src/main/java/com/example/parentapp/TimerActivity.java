5c8bab6f61643167c262624bbf81d7f79744e790
wla161 <wla161@sfu.ca>
2021-10-27 18:26:18 -0700

    TimerActivity: added ability to start, reset, pause and resume timer

--- app/src/main/java/com/example/parentapp/TimerActivity.java
+++ app/src/main/java/com/example/parentapp/TimerActivity.java
@@ -2,16 +2,129 @@ package com.example.parentapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class TimerActivity extends AppCompatActivity {
+    private TextView txtTimeCountDown;
+    private ImageButton btnStart;
+    private ImageButton btnReset;
+    private ImageButton btnPause;
+    private ImageButton btnResume;
+    private static final long DEFAULT_DISPLAY_TIMER_MILLIS = 600000;
+    private boolean isRunning = false;
+    private CountDownTimer countDownTimer;
+    private long timeLeftMills = DEFAULT_DISPLAY_TIMER_MILLIS;
+
+    public static Intent makeIntent(Context context) {
+        return new Intent(context,TimerActivity.class);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
-
         getSupportActionBar().setTitle(R.string.timer_title);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        txtTimeCountDown = findViewById(R.id.txtTimer);
+        btnStart = findViewById(R.id.imgBtnStart);
+        btnReset = findViewById(R.id.imgBtnReset);
+        btnPause = findViewById(R.id.imgBtnPause);
+        btnResume = findViewById(R.id.imgBtnResume);
+        btnStart.setVisibility(View.VISIBLE);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startTimer();
+                btnStart.setVisibility(View.INVISIBLE);
+                btnReset.setVisibility(View.VISIBLE);
+            }
+        });
+
+        btnResume.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startTimer();
+                btnResume.setVisibility(View.INVISIBLE);
+                btnPause.setVisibility(View.VISIBLE);
+                btnReset.setVisibility(View.VISIBLE);
+                btnStart.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        btnPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pauseTimer();
+                btnPause.setVisibility(View.INVISIBLE);
+                btnResume.setVisibility(View.VISIBLE);
+                btnReset.setVisibility(View.VISIBLE);
+                btnStart.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resetTimer();
+                pauseTimer();
+                btnStart.setVisibility(View.VISIBLE);
+                btnPause.setVisibility(View.INVISIBLE);
+                btnReset.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        refreshCountDownText();
+    }
+
+
+    private void startTimer() {
+        countDownTimer = new CountDownTimer(timeLeftMills,1000) {
+            @Override
+            public void onTick(long l) {
+                timeLeftMills = l;
+                refreshCountDownText();
+            }
+
+            @Override
+            public void onFinish() {
+                isRunning = false;
+                btnStart.setVisibility(View.INVISIBLE);
+                btnReset.setVisibility(View.VISIBLE);
+            }
+        }.start();
+        isRunning = true;
+        btnPause.setVisibility(View.VISIBLE);
+        btnReset.setVisibility(View.INVISIBLE);
+    }
+
+
+    private void resetTimer() {
+        timeLeftMills = DEFAULT_DISPLAY_TIMER_MILLIS;
+        refreshCountDownText();
+        btnReset.setVisibility(View.INVISIBLE);
+        btnResume.setVisibility(View.INVISIBLE);
+    }
+
+    private void pauseTimer() {
+        countDownTimer.cancel();
+        isRunning = false;
+    }
+
+    private void refreshCountDownText() {
+        int minutes = (int) timeLeftMills / 1000 / 60;
+        int seconds = (int) timeLeftMills / 1000 % 60;
+        String displayTimeLeft = String.format(Locale.getDefault(),"%02d:%02d", minutes, seconds);
+        txtTimeCountDown.setText(displayTimeLeft);
     }
 }
