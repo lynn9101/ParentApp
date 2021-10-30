b90dbd023e621a7384be14e7290c95c51e757120
jla787 <jla787@sfu.ca>
2021-10-29 22:34:44 -0700

    models/helper: added ability to store and retrieve objects from SharedPreference
    Works with both objects and List of objects. Does not work with list of lists though.
    
    Demo example:
    
            CoinFlip f1 = new CoinFlip(new Child(), false);
            Helpers.saveObjectToSharedPreference(getApplicationContext(), "kid1", f1);
            CoinFlip f11 = Helpers.getObjectFromSharedPreference(getApplicationContext(), "kid1", Helpers.getClassType(CoinFlip.class));
    
            ArrayList<CoinFlip> flips = new ArrayList<>();
            flips.add(new CoinFlip(new Child(), false));
            flips.add(new CoinFlip(new Child(), true));
            flips.add(new CoinFlip(new Child(), false));
            Helpers.saveObjectToSharedPreference(getApplicationContext(), "kids", flips);
    
            ArrayList<CoinFlip> flips2 = Helpers.getObjectFromSharedPreference(getApplicationContext(), "kids", Helpers.getListOfClassType(CoinFlip.class));

--- app/src/main/java/com/example/parentapp/models/Helpers.java
+++ app/src/main/java/com/example/parentapp/models/Helpers.java
@@ -1,9 +1,75 @@
 package com.example.parentapp.models;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.example.parentapp.R;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
 public class Helpers {
+
+    public static final Gson gson = new Gson();
 
     // Prevent Helpers from being instantiated.
     private Helpers() {
+    }
 
+    public static boolean isStringNullOrEmpty(String string) {
+        return string == null || string.length() == 0;
+    }
+
+    public static SharedPreferences getSharedPreference(Context context) {
+        String sharedPrefKey = context.getResources().getString(R.string.shared_pref_key);
+
+        return context.getSharedPreferences(sharedPrefKey, Context.MODE_PRIVATE);
+    }
+
+    public static SharedPreferences.Editor getSharedPrefEditor(Context context) {
+        SharedPreferences sharedPref = Helpers.getSharedPreference(context);
+
+        return sharedPref.edit();
+    }
+
+    public static void saveObjectToSharedPreference(Context context, String objectKey, Object object) {
+        if (object == null) {
+            throw new NullPointerException("Null object encountered at Helper -> saveObjectToSharedPreference.");
+        }
+
+        SharedPreferences.Editor sharedPrefEditor = getSharedPrefEditor(context);
+        sharedPrefEditor.putString(objectKey, gson.toJson(object));
+
+        sharedPrefEditor.apply();
+    }
+
+    public static <T> Type getClassType(Class<T> type) {
+        return TypeToken.get(type).getType();
+    }
+
+    public static <T> Type getListOfClassType(Class<T> type) {
+        return TypeToken.getParameterized(List.class, type).getType();
+    }
+
+    public static <T> T getObjectFromSharedPreference(Context context, String objectKey, Type type) {
+        SharedPreferences sharedPref = getSharedPreference(context);
+
+        if (!sharedPref.contains(objectKey)) {
+            throw new IllegalArgumentException("Helper -> getObjectFromSharedPreference: " + objectKey + " does not exist within the shared preference.");
+        }
+
+        String deserializeObject = sharedPref.getString(objectKey, null);
+
+        if (Helpers.isStringNullOrEmpty(deserializeObject)) {
+            throw new NullPointerException("Null or empty string encountered at Helper -> getObjectFromSharedPreference.");
+        }
+
+        if (type == null) {
+            throw new IllegalArgumentException("Helper -> getObjectFromSharedPreference: Type must be specified.");
+        }
+
+        return gson.fromJson(deserializeObject, type);
     }
 }
