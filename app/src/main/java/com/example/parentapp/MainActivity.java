8b98536ea39cbf0bfe7f73ac5c7129d9d4276c06
nnn2 <nnn2@sfu.ca>
2021-10-30 14:40:28 -0700

    MainActivity: removed action bar, set background image and fixed layout in xml activity_main.xml

--- app/src/main/java/com/example/parentapp/MainActivity.java
+++ app/src/main/java/com/example/parentapp/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().setTitle(R.string.main_title);
+        getSupportActionBar().hide();
 
         attachButtonListeners();
     }
