d8a59a658a6a7b2540fc894c771fd762f7f9f0db
Jla787 <Jla787@sfu.ca>
2021-11-01 15:06:32 -0700

    activities: added new activity coinfliphistory, as well as basic functionality to coinflipactivity
    The coinfliphistory activity records all past coinflip instances and compiles them into a custom listview
    Each row displays who got the pick, did the child pick head/tail and whether the child won or lost.
    
    The coinfliphistory supports pulling data from sharedpreference
    
    In addition, basic functionalites were added to the coinflip activity to test addition of coinflips

--- app/src/main/java/com/example/parentapp/CoinFlipActivity.java
+++ app/src/main/java/com/example/parentapp/CoinFlipActivity.java
@@ -2,9 +2,29 @@ package com.example.parentapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.parentapp.models.Child;
+import com.example.parentapp.models.CoinFlip;
+import com.example.parentapp.models.CoinFlipManager;
+import com.example.parentapp.models.Helpers;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class CoinFlipActivity extends AppCompatActivity {
+
+    private CoinFlipManager coinFlipManager;
+    private Random rng;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +33,62 @@ public class CoinFlipActivity extends AppCompatActivity {
 
         getSupportActionBar().setTitle(R.string.coin_flip_title);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        this.coinFlipManager = CoinFlipManager.getInstance();
+        this.rng = new Random();
+
+        attachButtonListeners();
+    }
+
+    private void attachButtonListeners() {
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CoinFlipActivity.this, CoinFlipHistoryActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button addHeadBtn = findViewById(R.id.addHead);
+
+        addHeadBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean result = rng.nextInt() % 2 == 0;
+                CoinFlip flip = new CoinFlip(new Child("a", "b"), result, true);
+                coinFlipManager.addCoinFlip(flip);
+                updateCoinFlipHistorySharedPref();
+            }
+        });
+
+        Button addTailBtn = findViewById(R.id.addTail);
+
+        addTailBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean result = rng.nextInt() % 2 == 0;
+                CoinFlip flip = new CoinFlip(new Child("a", "b"), result, false);
+                coinFlipManager.addCoinFlip(flip);
+                updateCoinFlipHistorySharedPref();
+            }
+        });
+
+        Button clearBtn = findViewById(R.id.clearCoinFlipHistory);
+
+        clearBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                coinFlipManager.resetCoinFlipHistory();
+                updateCoinFlipHistorySharedPref();
+            }
+        });
+
+    }
+
+    private void updateCoinFlipHistorySharedPref() {
+        Context ctx = getApplicationContext();
+        String historyKey = ctx.getResources().getString(R.string.shared_pref_coin_flip_history_key);
+        Helpers.saveObjectToSharedPreference(ctx, historyKey, coinFlipManager.getCoinFlipHistory());
     }
 }
---
+++ app/src/main/java/com/example/parentapp/CoinFlipHistoryActivity.java
@@ -0,0 +1,88 @@
+package com.example.parentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.example.parentapp.models.CoinFlip;
+import com.example.parentapp.models.CoinFlipManager;
+import com.example.parentapp.models.Helpers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CoinFlipHistoryActivity extends AppCompatActivity {
+
+    private List<CoinFlip> history = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coin_flip_history);
+
+        getSupportActionBar().setTitle(R.string.coin_flip_history_title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        populateCoinFlipHistoryList();
+    }
+
+    private void populateCoinFlipHistoryList() {
+        Context ctx = getApplicationContext();
+        CoinFlipManager coinManager = CoinFlipManager.getInstance();
+        SharedPreferences sharedPreference = Helpers.getSharedPreference(ctx);
+        String historyKey = ctx.getResources().getString(R.string.shared_pref_coin_flip_history_key);
+
+        if (sharedPreference.contains(historyKey)) {
+            coinManager.setCoinFlipHistory(Helpers.getObjectFromSharedPreference(ctx, historyKey, Helpers.getListOfClassType(CoinFlip.class)));
+        }
+
+        this.history = coinManager.getCoinFlipHistory();
+        ListView historyList = findViewById(R.id.coinflipHistoryList);
+        ArrayAdapter<CoinFlip> adapter = new CoinFlipHistoryListAdapter();
+
+        historyList.setAdapter(adapter);
+    }
+
+    private class CoinFlipHistoryListAdapter extends ArrayAdapter<CoinFlip> {
+        public CoinFlipHistoryListAdapter() {
+            super(CoinFlipHistoryActivity.this, R.layout.coinflip_history_list, history);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View itemView = convertView;
+            if (itemView == null) {
+                itemView = getLayoutInflater().inflate(R.layout.coinflip_history_list, parent, false);
+            }
+
+            CoinFlip coinFlipInstance = history.get(position);
+
+            ImageView statusIcon = (ImageView)itemView.findViewById(R.id.statusIcon);
+            int iconID;
+            if (coinFlipInstance.didPickerWin()) {
+                iconID = R.drawable.ic_check_circle_48;
+            } else {
+                iconID = R.drawable.ic_remove_circle_48;
+            }
+            statusIcon.setImageResource(iconID);
+
+            TextView pickerStatus = (TextView) itemView.findViewById(R.id.pickerStatus);
+            pickerStatus.setText(coinFlipInstance.getPickerStatus());
+
+            TextView coinFlipDate = (TextView) itemView.findViewById(R.id.coinFlipDate);
+            coinFlipDate.setText(coinFlipInstance.getFormattedCoinFlipTime());
+
+            return itemView;
+        }
+    }
+
+}
