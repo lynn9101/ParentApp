2893b78c108c6bf07e0b2a7f9574f967cfc27f0f
nnn2 <nnn2@sfu.ca>
2021-10-31 23:58:13 -0700

    ChildrenActivity: displayed text when there is no children in the list view.

--- app/src/main/java/com/example/parentapp/ChildrenActivity.java
+++ app/src/main/java/com/example/parentapp/ChildrenActivity.java
@@ -5,10 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.example.parentapp.models.Child;
+import com.example.parentapp.models.ChildrenManager;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+
 public class ChildrenActivity extends AppCompatActivity {
+    private ChildrenManager childrenManager = ChildrenManager.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +26,11 @@ public class ChildrenActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         attachButtonListeners();
-        // test
-        attachButtonListeners();
+        displayEmptyList();
     }
 
     private void attachButtonListeners() {
-        FloatingActionButton fab = findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.addButton);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -32,5 +38,18 @@ public class ChildrenActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void displayEmptyList() {
+        TextView noChildText = findViewById(R.id.noChildText);
+        ImageView addIcon = findViewById(R.id.addIcon);
+        TextView addInstruction = findViewById(R.id.addInstruction);
+
+        ArrayList<Child> childrenList = childrenManager.getChildren();
+        if(childrenList.size() != 0) {
+            noChildText.setVisibility(View.INVISIBLE);
+            addIcon.setVisibility(View.INVISIBLE);
+            addInstruction.setVisibility(View.INVISIBLE);
+        }
     }
 }
--- app/src/main/java/com/example/parentapp/models/Child.java
+++ app/src/main/java/com/example/parentapp/models/Child.java
@@ -1,8 +1,13 @@
 package com.example.parentapp.models;
 
 public class Child {
+    private final String CHILD_NAME;
 
-    public Child() {
+    public Child(String name) {
+        CHILD_NAME = name;
+    }
 
+    public String getName() {
+        return CHILD_NAME;
     }
 }
