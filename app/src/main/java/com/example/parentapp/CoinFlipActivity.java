cdaa475ae9062b4eb32be857960080a8356ef9e5
nnn2 <nnn2@sfu.ca>
2021-11-06 00:37:05 -0700

    CoinFlipActivity: added drop down list to select the child.

--- app/src/main/java/com/example/parentapp/CoinFlipActivity.java
+++ app/src/main/java/com/example/parentapp/CoinFlipActivity.java
@@ -12,19 +12,24 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.parentapp.models.Child;
+import com.example.parentapp.models.ChildrenManager;
 import com.example.parentapp.models.CoinFlip;
 import com.example.parentapp.models.CoinFlipManager;
 import com.example.parentapp.models.Helpers;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class CoinFlipActivity extends AppCompatActivity {
-
+    private ChildrenManager childrenManager;
+    private List<Child> childrenList = new ArrayList<>();
+    private List<String> childrenFullNames = new ArrayList<>();
     private CoinFlipManager coinFlipManager;
     private Random rng;
 
@@ -36,28 +41,39 @@ public class CoinFlipActivity extends AppCompatActivity {
         getSupportActionBar().setTitle(R.string.coin_flip_title);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        this.childrenManager = ChildrenManager.getInstance();
         this.coinFlipManager = CoinFlipManager.getInstance();
         this.rng = new Random();
 
-        flipCoinAnimation();
+        populateChildrenList();
+        displayDropDownList();
         attachButtonListeners();
     }
 
-    private void flipCoinAnimation() {
-        ImageView coinHead = findViewById(R.id.coinHead);
+    private void populateChildrenList() {
+        Context context = getApplicationContext();
+        SharedPreferences sharedPreferences = Helpers.getSharedPreference(context);
+        String childrenListKey = context.getResources().getString(R.string.shared_pref_children_list_key);
 
-        coinHead.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                RotateAnimation rotateAnimation = new RotateAnimation(0, -360, RotateAnimation.RELATIVE_TO_SELF,
-                        .5f, RotateAnimation.RELATIVE_TO_SELF, .5f);
+        if (sharedPreferences.contains(childrenListKey)) {
+            childrenManager.setChildren(Helpers.getObjectFromSharedPreference(context, childrenListKey, Helpers.getListOfClassType(Child.class)));
+        }
 
-                rotateAnimation.setDuration(1000);
-                coinHead.startAnimation(rotateAnimation);
-            }
-        });
-
+        this.childrenList = childrenManager.getChildren();
+        for (int i = 0; i < childrenList.size(); i++) {
+            Child childInInstance = childrenList.get(i);
+            String fullName = childInInstance.getFirstName() + " " + childInInstance.getLastName();
+            childrenFullNames.add(fullName);
+        }
     }
+
+    private void displayDropDownList() {
+        Spinner spinner = findViewById(R.id.childrenListPicked);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(CoinFlipActivity.this,
+                R.layout.support_simple_spinner_dropdown_item, childrenFullNames);
+        spinner.setAdapter(adapter);
+    }
+
 
     private void attachButtonListeners() {
         FloatingActionButton fab = findViewById(R.id.fab);
