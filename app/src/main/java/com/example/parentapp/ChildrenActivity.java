e05c1ef9ed84a9ac863c34ce6dd64e3a2f91c2ee
nnn2 <nnn2@sfu.ca>
2021-11-05 21:53:42 -0700

    ChildrenActivity: deleted unused libraries.

--- app/src/main/java/com/example/parentapp/ChildrenActivity.java
+++ app/src/main/java/com/example/parentapp/ChildrenActivity.java
@@ -5,8 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
