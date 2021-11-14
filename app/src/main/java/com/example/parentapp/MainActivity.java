47a94fb6027210fe0428593f9a5c380f938b3d0e
nnn2 <nnn2@sfu.ca>
2021-11-14 00:30:17 -0800

    MainActivity.java: set Floating Button that links to Help Screen Activity to be on click.

--- app/src/main/java/com/example/parentapp/MainActivity.java
+++ app/src/main/java/com/example/parentapp/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.cardview.widget.CardView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 /**
@@ -62,6 +63,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Button helpActivityBtn = findViewById(R.id.helpActivityButton);
+        helpActivityBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+            }
+        });
     }
 }
