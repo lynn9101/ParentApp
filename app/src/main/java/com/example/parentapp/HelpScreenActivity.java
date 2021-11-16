5418c09a6fee0db7269e4e4b8ccedf0eab2c6f47
fkiannej <fkiannej@sfu.ca>
2021-11-15 18:25:22 -0800

    Change the help page so it looks better
    Links are now clickable
    Long text section is distinguished
    New font timeless.ttf added
    New XML file back.xml added

--- app/src/main/java/com/example/parentapp/HelpScreenActivity.java
+++ app/src/main/java/com/example/parentapp/HelpScreenActivity.java
@@ -6,6 +6,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class HelpScreenActivity extends AppCompatActivity {
 
@@ -22,10 +26,44 @@ public class HelpScreenActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources()
                 .getColor(R.color.app_title_color)));
+
+        setInitialStates();
+
+        TextView textForLongTxt = findViewById(R.id.textLongBox);
+        TextView textForTitle = findViewById(R.id.textTitleOfText);
+
+        ToggleButton toggleButtonForMemOrCopy  = findViewById(R.id.toggleMemOrCopy);
+        toggleButtonForMemOrCopy.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    textForTitle.setText(R.string.membersName);
+                    textForLongTxt.setText(R.string.listOfMembers);
+                } else {
+                    textForTitle.setText(R.string.copyrightName);
+                    textForLongTxt.setText(R.string.very_long_text);
+                }
+            }
+        });
     }
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
+    }
+
+    private void setInitialStates(){
+        ToggleButton toggleButtonForMemOrCopy  = findViewById(R.id.toggleMemOrCopy);
+        toggleButtonForMemOrCopy.setText(getString(R.string.copyrightName));
+        toggleButtonForMemOrCopy.setTextOff(getString(R.string.membersName));
+        toggleButtonForMemOrCopy.setTextOn(getString(R.string.copyrightName));
+
+        TextView textForLongTxt = findViewById(R.id.textLongBox);
+        TextView textForTitle = findViewById(R.id.textTitleOfText);
+
+        textForLongTxt.setMovementMethod(new ScrollingMovementMethod());
+        textForLongTxt.setText(R.string.listOfMembers);
+        textForTitle.setText(R.string.membersName);
+        textForTitle.setAllCaps(true);
     }
 }
